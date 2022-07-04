6564898494bbf9b5f43c795e1dcd6b2abcd16f95
ver217 <lhx0217@gmail.com>
2022-07-04 19:06:30 +0800

    add unit test

---
+++ tests/test_offloader.py
@@ -0,0 +1,34 @@
+import torch
+from functools import partial
+from colo_nvme import Offloader
+
+
+def test_eq():
+    x = torch.rand(2, 2)
+    x_copy = x.clone()
+    of = Offloader('test.pth', 4)
+    of.write(x, str(id(x)))
+    of.synchronize()
+    x.zero_()
+    assert not torch.equal(x, x_copy)
+    of.read(x, str(id(x)))
+    of.synchronize()
+    assert torch.equal(x, x_copy)
+
+
+def test_callback():
+    x = torch.rand(2, 2)
+    x_copy = x.clone()
+    of = Offloader('test.pth', 4)
+    callback = partial(x.storage().resize_, 0)
+    of.write(x, str(id(x)), callback)
+    of.synchronize()
+    assert x.storage().size() == 0
+    x.storage().resize_(x.numel())
+    of.read(x, str(id(x)))
+    assert torch.equal(x, x_copy)
+
+
+if __name__ == '__main__':
+    test_eq()
+    test_callback()
