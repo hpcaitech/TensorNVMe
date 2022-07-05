0587cdbf0b3640ea290967d2922274c8494b0789
zxl <43881818+oahzxl@users.noreply.github.com>
2022-07-05 12:05:52 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ from torch.utils.cpp_extension import CppExtension, BuildExtension
 
 setup(
     name='colo_nvme',
+    packages=find_packages(exclude=(
+        'csrc',
+        'tests',
+        '*.egg-info'
+    )),
     ext_modules=[CppExtension('colo_nvme', ['csrc/offload.cpp', 'csrc/uring.cpp', 'csrc/aio.cpp', 'csrc/space_mgr.cpp'],
                               include_dirs=['csrc'],
                               extra_compile_args=['-luring', '-aio'],
                               libraries=['uring', 'aio'])],
-    # cmdclass={'build_ext': BuildExtension}
+    cmdclass={'build_ext': BuildExtension}
 )
