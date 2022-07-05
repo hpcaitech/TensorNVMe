dc7ab2733fa3732541d4eeb4866028c3fc6f6334
zxl <43881818+oahzxl@users.noreply.github.com>
2022-07-05 12:02:53 +0800

    add aio in setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from torch.utils.cpp_extension import CppExtension, BuildExtension
 
 setup(
     name='colo_nvme',
-    ext_modules=[CppExtension('colo_nvme', ['csrc/offload.cpp', 'csrc/uring.cpp', 'csrc/space_mgr.cpp'],
+    ext_modules=[CppExtension('colo_nvme', ['csrc/offload.cpp', 'csrc/uring.cpp', 'csrc/aio.cpp', 'csrc/space_mgr.cpp'],
                               include_dirs=['csrc'],
-                              extra_compile_args=['-luring'],
-                              libraries=['uring'])],
+                              extra_compile_args=['-luring', '-aio'],
+                              libraries=['uring', 'aio'])],
     # cmdclass={'build_ext': BuildExtension}
 )
