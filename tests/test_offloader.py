b70dd4cc9012e665090672e2607737c2836aa790
ver217 <lhx0217@gmail.com>
2022-07-05 15:44:13 +0800

    update unit tests

--- tests/test_offloader.py
+++ tests/test_offloader.py
@@ -1,17 +1,17 @@
 import torch
 from functools import partial
-from colo_nvme import Offloader
+from colo_nvme._C import Offloader
 
 
 def test_eq():
     x = torch.rand(2, 2)
     x_copy = x.clone()
     of = Offloader('test.pth', 4)
-    of.write(x, str(id(x)))
+    of.async_write(x, str(id(x)))
     of.synchronize()
     x.zero_()
     assert not torch.equal(x, x_copy)
-    of.read(x, str(id(x)))
+    of.async_read(x, str(id(x)))
     of.synchronize()
     assert torch.equal(x, x_copy)
 
@@ -21,11 +21,11 @@ def test_callback():
     x_copy = x.clone()
     of = Offloader('test.pth', 4)
     callback = partial(x.storage().resize_, 0)
-    of.write(x, str(id(x)), callback)
+    of.async_write(x, str(id(x)), callback)
     of.synchronize()
     assert x.storage().size() == 0
     x.storage().resize_(x.numel())
-    of.read(x, str(id(x)))
+    of.async_read(x, str(id(x)))
     assert torch.equal(x, x_copy)
 
 
