87bb2ab2fe385e9f1c680bf7c286e5310383c07d
ver217 <lhx0217@gmail.com>
2022-07-05 17:01:33 +0800

    support register file

--- csrc/aio.cpp
+++ csrc/aio.cpp
@@ -10,6 +10,8 @@ AIOAsyncIO::AIOAsyncIO(int max_events)
     this->timeout.tv_nsec = 100000000;
 }
 
+void AIOAsyncIO::register_file(int fd) {}
+
 AIOAsyncIO::~AIOAsyncIO()
 {
     // printf("Closing AsyncIO context\n");
@@ -19,15 +21,15 @@ AIOAsyncIO::~AIOAsyncIO()
 
 void AIOAsyncIO::wait()
 {
-    auto* events = new io_event[this->max_nr]; /* pending I/O max support */
+    auto *events = new io_event[this->max_nr]; /* pending I/O max support */
     int num_events;
 
     num_events = io_getevents(io_ctx, this->min_nr, this->max_nr, events, &(this->timeout)); /* 获得异步I/O event个数 */
 
-    for(int i = 0; i < num_events; i++) /* 开始获取每一个event并且做相应处理 */
+    for (int i = 0; i < num_events; i++) /* 开始获取每一个event并且做相应处理 */
     {
         struct io_event event = events[i];
-        auto* data = (IOData*)event.data;
+        auto *data = (IOData *)event.data;
         if (data->type == WRITE)
             this->n_write_events--;
         else if (data->type == READ)
@@ -42,25 +44,29 @@ void AIOAsyncIO::wait()
 
 void AIOAsyncIO::write(int fd, void *buffer, size_t n_bytes, unsigned long long offset, callback_t callback)
 {
-    struct iocb iocb{}; //建立一个异步I/O需求
-    struct iocb* iocbs = &iocb;
+    struct iocb iocb
+    {
+    }; //建立一个异步I/O需求
+    struct iocb *iocbs = &iocb;
     auto *data = new IOData(WRITE, callback);
 
-    io_prep_pwrite(&iocb, fd, buffer, n_bytes, (long long)offset);  // 初始化这个异步I/O需求 counter为偏移量
+    io_prep_pwrite(&iocb, fd, buffer, n_bytes, (long long)offset); // 初始化这个异步I/O需求 counter为偏移量
 
     data->type = WRITE;
     data->iov.iov_base = buffer;
     data->iov.iov_len = n_bytes;
     iocb.data = data;
-    io_submit(this->io_ctx, 1, &iocbs);             // 提交这个I/O不会堵塞
+    io_submit(this->io_ctx, 1, &iocbs); // 提交这个I/O不会堵塞
 
     this->n_write_events++;
 }
 
 void AIOAsyncIO::read(int fd, void *buffer, size_t n_bytes, unsigned long long offset, callback_t callback)
 {
-    struct iocb iocb{};//建立一个异步I/O需求
-    struct iocb* iocbs = &iocb;
+    struct iocb iocb
+    {
+    }; //建立一个异步I/O需求
+    struct iocb *iocbs = &iocb;
     auto *data = new IOData(READ, callback);
 
     io_prep_pwrite(&iocb, fd, buffer, n_bytes, (long long)offset);
@@ -69,7 +75,7 @@ void AIOAsyncIO::read(int fd, void *buffer, size_t n_bytes, unsigned long long o
     data->iov.iov_base = buffer;
     data->iov.iov_len = n_bytes;
     iocb.data = data;
-    io_submit(this->io_ctx,1, &iocbs); /* 提交这个I/O不会堵塞 */
+    io_submit(this->io_ctx, 1, &iocbs); /* 提交这个I/O不会堵塞 */
 
     this->n_read_events++;
 }
