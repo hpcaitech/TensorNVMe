95dd962bf7669dbb70515c13bcdb07426c349569
ver217 <lhx0217@gmail.com>
2022-07-18 15:23:58 +0800

    fix setup

--- setup.py
+++ setup.py
@@ -15,10 +15,13 @@ def cpp_ext_helper(name, sources, **kwargs):
 
 
 extra_compile_args = []
+libraries = ['uring', 'aio']
 if os.environ.get('DISABLE_URING') == '1':
     extra_compile_args.append('-DDISABLE_URING')
+    libraries.remove('uring')
 if os.environ.get('DISABLE_AIO') == '1':
     extra_compile_args.append('-DDISABLE_AIO')
+    libraries.remove('aio')
 
 setup(
     name='colo_nvme',
@@ -29,6 +32,6 @@ setup(
     )),
     ext_modules=[cpp_ext_helper('colo_nvme._C', ['csrc/offload.cpp', 'csrc/uring.cpp', 'csrc/aio.cpp', 'csrc/space_mgr.cpp'],
                                 extra_compile_args=extra_compile_args,
-                                libraries=['uring', 'aio'])],
+                                libraries=libraries)],
     cmdclass={'build_ext': BuildExtension}
 )
