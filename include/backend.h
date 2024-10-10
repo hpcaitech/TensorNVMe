21361884d5d9f0a89485fc80009b17b4936b9d74
botbw <wang1570@e.ntu.edu.sg>
2024-10-10 11:16:13 +0800

    [backend] add debug flag

--- include/backend.h
+++ include/backend.h
@@ -1,5 +1,7 @@
 #include "asyncio.h"
 #include <string>
+#include <algorithm>
+#include <cctype>
 #include <unordered_set>
 #include <cstdlib>
 #include <iostream>
@@ -9,5 +11,7 @@ std::unordered_set<std::string> get_backends();
 bool probe_backend(const std::string &backend);
 
 std::string get_default_backend();
+
+bool get_debug_flag();
 
 AsyncIO *create_asyncio(unsigned int n_entries, std::string backend);
